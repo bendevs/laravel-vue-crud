9e104aa1410100664b755b78f356db7c1bd8eb9b
Luis <hi@luisbits.com>
2018-06-12 15:28:50 -0500

    Agregando formulario y seccion para pensamiento

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,9 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">
+                    ¿En qué estás pensando?
+                </div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -13,8 +15,30 @@
                             {{ session('status') }}
                         </div>
                     @endif
+                    <form action="" class="form">
+                        <div class="form-group">
+                            <label for="thought">Ahora estoy pensando en:</label>
+                            <input type="text" name="thought" class="form-control" id="">
+                        </div>
+                        <button class="btn btn-primary">
+                                Enviar pensamiento <span class="badge badge-primary"></span>
+                        </button>
+                    </form>
+                </div>
+            </div>
 
-                    You are logged in!
+            <div class="card">
+                <div class="card-header">
+                    Publicado en 12/Junio/2018
+                </div>
+                <div class="card-body">
+                    <p class="card-text">
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non voluptate nemo enim numquam consequatur rerum totam illo, quasi ipsam dolores temporibus iure rem saepe cupiditate explicabo possimus repellendus tempora tenetur.
+                    </p>
+                </div>
+                <div class="card-footer text-muted">
+                    <button type="button" class="btn btn-primary">Editar</button>
+                    <button type="button" class="btn btn-danger">Eliminar</button>
                 </div>
             </div>
         </div>
